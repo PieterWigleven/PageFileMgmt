9bc2b8d7490222d3dfedb0565a16c6c7ffc1f3b0
pietermsft <54368644+pietermsft@users.noreply.github.com>
2019-09-26 11:18:46 -0700

    Add files via upload

---
+++ ManagePageFile.ps1
@@ -0,0 +1,28 @@
+﻿{
+    "apiVersion": "2018-06-01",
+    "type": "Microsoft.Compute/virtualMachines/extensions",
+    "name": "virtualMachineName/config-app",
+    "location": "[resourceGroup().location]",
+    "dependsOn": [
+        "[concat('Microsoft.Compute/virtualMachines/', variables('vmName'),copyindex())]",
+        "[variables('musicstoresqlName')]"
+    ],
+    "tags": {
+        "displayName": "config-app"
+    },
+    "properties": {
+        "publisher": "Microsoft.Compute",
+        "type": "CustomScriptExtension",
+        "typeHandlerVersion": "1.9",
+        "autoUpgradeMinorVersion": true,
+        "settings": {
+            "fileUris": [
+                "https://raw.githubusercontent.com/pietermsft/PageFileMgmt/master/AdjustVirtualMemoryPagingFileSize.psm1"
+            ],
+            "timestamp":123456789
+        },
+        "protectedSettings": {
+            "commandToExecute": "powershell -ExecutionPolicy Unrestricted; Import-Module AdjustVirtualMemoryPagingFileSize.psm1; Set-OSCVirtualMemory -InitialSize 15360 -MaximumSize 15360 -DriveLetter "D:""
+            }
+    }
+}
